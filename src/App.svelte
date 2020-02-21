6ab99271d03c666b37f49ee52a9bb79f9c8d486d
aftenig <igor.afteni@ext.ec.europa.eu>
2020-02-21 15:09:05 +0100

    test fetch

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,14 @@
 <script>
   import { Router } from "@sveltech/routify";
   import { routes } from "@sveltech/routify/tmp/routes";
+
+  function getTranslations(lang) {
+    fetch('/static/lang.json')
+            .then(result => result.json())
+            .then(translation => console.log(translation));
+  }
+
+  getTranslations('en');
 </script>
 
 <Router {routes} />
